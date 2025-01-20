2028f4a9067872801d4dbf7e99a9ccb1e161fe19
rafaseto <rafael08.0807@gmail.com>
2025-01-20 16:12:48 -0300

    feat: count swaps and calls (hoare.cpp)

--- quicksort/hoare.cpp
+++ quicksort/hoare.cpp
@@ -4,28 +4,32 @@
 using namespace std;
 
 
-int hoare(vector<int>&arr, int ini, int fim) {
+int hoare(vector<int>&arr, int ini, int fim, int& num_trocas_chamadas) {
     int P = arr[ini], i = ini - 1, j = fim + 1;
 
     while (1) {
         while (arr[--j] > P);
         while (arr[++i] < P);
-        if (i < j) swap(arr[i], arr[j]);
+        if (i < j) {
+            num_trocas_chamadas++;
+            swap(arr[i], arr[j]);   
+        }
         else return j;
     }
 }
 
-void quicksort(vector<int>&arr, int ini, int fim) {
+void quicksort(vector<int>&arr, int ini, int fim, int& num_trocas_chamadas) {
+    num_trocas_chamadas++;
     if (ini < fim) {
-        int pivo = hoare(arr, ini, fim);
+        int pivo = hoare(arr, ini, fim, num_trocas_chamadas);
 
-        quicksort(arr, ini, pivo);
-        quicksort(arr, pivo + 1, fim);
+        quicksort(arr, ini, pivo, num_trocas_chamadas);
+        quicksort(arr, pivo + 1, fim, num_trocas_chamadas);
     }
 }
 
 int main() {
-    vector<int> arr = {847, 38, -183, -13, 94, -2, -42, 54, 28, 100};
+    vector<int> arr = {-23, 10, 7, -34, 432, 3};
 
     int num_trocas_chamadas = 0;
 
@@ -35,7 +39,7 @@ int main() {
     }
     cout << endl;
 
-    quicksort(arr, 0 , arr.size() - 1);
+    quicksort(arr, 0 , arr.size() - 1, num_trocas_chamadas);
 
     cout << "Ordered vector: ";
     for (int num : arr) {
@@ -44,7 +48,7 @@ int main() {
     cout << endl;
 
     // Exibir as estatísticas
-    // cout << "Swaps and Calls: " << num_trocas_chamadas << endl;
+    cout << "Swaps and Calls: " << num_trocas_chamadas << endl;
 
     return 0;
 }
