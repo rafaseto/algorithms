9c344592854d2f97de1dff07d5ee7bb36dd3d3fe
rafaseto <rafael08.0807@gmail.com>
2025-01-27 17:22:59 -0300

    feat: change round

--- mergesort/porto02.cpp
+++ mergesort/porto02.cpp
@@ -29,7 +29,7 @@ int32_t arredondarSimples(double numero) {
 bool containerOk(const Container& cadastrado, const Container& selecionado) {
     if (cadastrado.cnpj != selecionado.cnpj) 
         return false;
-    else if (selecionado.peso > cadastrado.peso * 1.1)
+    else if ((selecionado.peso >= cadastrado.peso * 1.105) || (selecionado.peso <= cadastrado.peso * 0.895))
         return false;
     else 
         return true;
@@ -58,12 +58,12 @@ Container separaContaineresNaoOk(Container* cadastrados, int32_t qtdeCadastrados
     }
 }
 
-// Função para copiar elementos de um vetor para outro
+// Func para copiar elementos de um vetor para outro
 void copiar(Fiscalizacao* destino, Fiscalizacao* origem, int32_t tamanho) {
     std::copy(origem, origem + tamanho, destino);
 }
 
-// Função para intercalar dois subarrays ordenados
+// Func para intercalar dois subarrays ordenados
 void intercalar(Fiscalizacao* vetorAux, Fiscalizacao* vetorOriginal, int32_t iniSubArr01, int32_t fimSubArr01, int32_t fimSubArr02) {
     int32_t i1 = iniSubArr01;     // Índice do primeiro subarray
     int32_t i2 = fimSubArr01 + 1; // Índice do segundo subarray
@@ -76,7 +76,7 @@ void intercalar(Fiscalizacao* vetorAux, Fiscalizacao* vetorOriginal, int32_t ini
 
 
 
-    // Intercala os elementos dos subarrays ordenados
+    // func p intercalar os elementos dos subarrays ordenados
     while (i1 <= fimSubArr01 && i2 <= fimSubArr02) {
         // CNPJ DIVERGENTE TEM PRIORIDADE
         if (vetorOriginal[i1].prioridade < vetorOriginal[i2].prioridade)
@@ -149,7 +149,7 @@ void intercalar(Fiscalizacao* vetorAux, Fiscalizacao* vetorOriginal, int32_t ini
     copiar(&vetorOriginal[iniSubArr01], &vetorAux[iniSubArr01], fimSubArr02 - iniSubArr01 + 1);
 }
 
-// Função Merge Sort recursiva
+// Func Merge Sort recursiva
 void mergesort(Fiscalizacao* vetorAux, Fiscalizacao* vetorOriginal, int32_t iniSubArr01, int32_t fimSubArr02) {
     if (iniSubArr01 < fimSubArr02) {
         int32_t fimSubArr01 = iniSubArr01 + (fimSubArr02 - iniSubArr01) / 2;
@@ -163,7 +163,7 @@ void mergesort(Fiscalizacao* vetorAux, Fiscalizacao* vetorOriginal, int32_t iniS
     }
 }
 
-// Função principal para testar o Merge Sort
+
 int main(int argc, char* argv[]) {
 
     if (argc < 3) {
